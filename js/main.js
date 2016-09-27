d0626ea9b253750ca77bad9326446d699bee917e
AndrewKamenev <Ancam@yandex.ru>
2016-09-27 21:28:47 +0300

    finish main part of project
    add all pages of project
    some bugs with adaptive, but it's work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function){
-
+$(document).ready(function(){
+	$('.button-adaptive').click(function(){
+		$('.header-nav').show();
+	})
+	$('.button-close-adaptive').click(function(){
+		$('.header-nav').hide();
+	})
 });
